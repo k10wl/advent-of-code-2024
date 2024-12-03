7bb757c46513afa9a964241b658f2cda4c19bce0
k10wl <aleks.antonsheko@gmail.com>
2024-12-03 21:53:03 +0200

    custom parsing goes brrrrrrrrrrrrrrrrrrrrrRR

--- 3/main.go
+++ 3/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"fmt"
 	"io"
+	"math"
 	"os"
 	"regexp"
 	"strconv"
+	"strings"
 )
 
 func main() {
@@ -13,7 +15,7 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	mulDataList, err := parseData(cleanup(data))
+	mulDataList, err := parseData([]byte(purgeBetween(string(data), "don't()", "do()")))
 	if err != nil {
 		panic(err)
 	}
@@ -27,6 +29,62 @@ func main() {
 type mulDataContainer struct {
 	a int
 	b int
+}
+
+func purgeBetween(content string, start string, end string) string {
+	removed := content
+	p1 := -1
+	p2 := -1
+	for i := range removed {
+		if p1 == -1 {
+			for j, jChar := range start {
+				next := i + j
+				if next >= len(removed) || string((removed[next])) != string(jChar) {
+					p1 = -1
+					break
+				}
+				if p1 != -1 {
+					continue
+				}
+				p1 = i
+			}
+			continue
+		}
+
+		if p1 == -1 || p1+len(start)-1 >= i {
+			continue
+		}
+
+		if p2 == -1 {
+			for j, jChar := range end {
+				next := i + j
+				if next >= len(removed) || string(removed[next]) != string(jChar) {
+					p2 = -1
+					break
+				}
+				p2 = next
+			}
+			continue
+		}
+
+		if p2 == -1 {
+			continue
+		}
+
+		removed = replaceBetween(removed, p1, p2, "#")
+		p1 = -1
+		p2 = -1
+	}
+	if p1 != -1 {
+		removed = replaceBetween(removed, p1, len(removed)-1, "#")
+	}
+	return removed
+}
+
+func replaceBetween(original string, p1 int, p2 int, replacement string) string {
+	start := original[0:p1]
+	end := original[p2:]
+	return start + strings.Repeat(replacement, int(math.Abs(float64(p2-p1)))) + end
 }
 
 func cleanup(content []byte) []byte {
